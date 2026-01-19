fbc20a119a561c479ec611675b0e09dccce53555
GeraRamirez <gerardoramirez656@gmail.com>
2026-01-19 18:41:09 -0300

    change names

--- src/features/fraud-panel/index.tsx
+++ src/features/fraud-panel/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const FraudPanel=()=> {
   return (
     <div>
--- src/features/overview/index.tsx
+++ src/features/overview/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { ContactButton } from './components'
 
 export const Overview=()=> {
